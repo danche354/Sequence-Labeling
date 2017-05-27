46b93d5d4815a976d245867df50dbac4d3be45b4
danche354 <pengzonghui@foxmail.com>
2017-05-27 13:15:17 +0800

    add random for chunk

--- tools/produce_chunk_ner_random.py
+++ tools/produce_chunk_ner_random.py
@@ -12,7 +12,7 @@ np.random.seed(0)
 chunk_hash_vocab = conf.chunk_hash_vocab
 chunk_hash_length = conf.chunk_hash_length
 
-chunk_embedding = np.random.rand(chunk_hash_vocab+1, chunk_hash_length)
+chunk_embedding = np.random.rand(chunk_hash_vocab, chunk_hash_length)
 # chunk_embedding.astype(np.float32)
 
 np.savetxt("../preprocessing/random/chunk_embeddings.txt", chunk_embedding, fmt="%.7f", delimiter=" ")
@@ -21,7 +21,7 @@ np.savetxt("../preprocessing/random/chunk_embeddings.txt", chunk_embedding, fmt=
 ner_hash_vocab = conf.ner_hash_vocab
 ner_hash_length = conf.ner_hash_length
 
-ner_embedding = np.random.rand(ner_hash_vocab+1, ner_hash_length)
+ner_embedding = np.random.rand(ner_hash_vocab, ner_hash_length)
 # ner_embedding.dtype="np.float64"
 
 np.savetxt("../preprocessing/random/ner_embeddings.txt", ner_embedding, fmt="%.7f", delimiter=" ")
